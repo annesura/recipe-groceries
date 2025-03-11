604a92eaac9cb17948a010fc9f11e2d7b52520d2
annesura <annesura00@gmail.com>
2025-03-11 14:42:51 -0400

    Finished adding recipe data

--- my-app/src/Data.js
+++ my-app/src/Data.js
@@ -1,5 +1,50 @@
 export const recipes = [{
     id: 'chicken-and-rice',
     name: 'Chicken and rice',
-    ingredients: ['chicken', 'white rice', 'cream of mushroom soup',]
-}];
+    ingredients: ['chicken', 'white rice', 'mushroom soup', 'bread (dinner)', 'teryaki sauce', 'beer', 'frozen vegetables']
+}, {
+    id: 'spaghetti',
+    name: 'Spaghetti',
+    ingredients: ['pasta', 'meat', 'pasta sauce', 'bread (dinner)', 'frozen vegetables']
+}, {
+    id: 'chicken-and-wild-rice',
+    name: 'Chicken and Wild Rice',
+    ingredients: ['chicken', 'wild rice', 'frozen vegetables']
+}, {
+    id: 'chicken-pesto-pasta',
+    name: 'Chicken Pesto Pasta',
+    ingredients: ['chicken', 'pasta', 'pesto', 'bread (dinner)']
+}, {
+    id: 'mac-and-cheese',
+    name: 'Mac & Cheese',
+    ingredients: ['mac & cheese', 'frozen vegetables']
+}, {
+    id: 'burgers',
+    name: 'Burgers',
+    ingredients: ['burger patties', 'burger buns', 'cheese', 'lettuce', 'wavy chips', 'baked beans', 'ketchup']
+}, {
+    id: 'breakfast',
+    name: 'breakfast',
+    ingredients: ['pancakes', 'eggs', 'bacon', 'potatoes']
+}, {
+    id: 'grilled-cheese',
+    name: 'Grilled Cheese',
+    ingredients: ['bread', 'kraft singles', 'bacon', 'soup']
+}, {
+    id: 'lemon-pepper-chicken',
+    name: 'Lemon Pepper Chicken',
+    ingredients: ['chicken', 'frozen vegetables', 'bread (dinner)']
+}, {
+    id: 'egg-burritos',
+    name: 'Egg Burritos',
+    ingredients: ['tortillas', 'eggs', 'bacon', 'cheese']
+}, {
+    id: 'pizza',
+    name: 'Pizza',
+    ingredients: ['frozen pizza']
+}, {
+    id: 'tacos',
+    name: 'Tacos',
+    ingredients: ['meat', 'rice', 'beans', 'rotel', 'onion', 'tortillas', 'tortilla chips', 'guac']
+}
+];
